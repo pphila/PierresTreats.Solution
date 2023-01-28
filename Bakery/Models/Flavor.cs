2ce39dba8831f897af562e4c80e9375543e6d3c0
pphila <efl2ice89@gmail.com>
2023-01-27 22:15:40 -0800

    add required fields to models

--- Bakery/Models/Flavor.cs
+++ Bakery/Models/Flavor.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
-// using System.ComponentModel.DataAnnotation;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bakery.Models
 {
   public class Flavor
   {
     public int FlavorId { get; set; }
+    [Required(ErrorMessage = "Field Cannot Be Empty!")]
     public string Type { get; set; }
     public List<TreatFlavor> JoinEntities { get; set; }
   }

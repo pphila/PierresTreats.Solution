8cc64e62cf05b4efe114babc32fbe618fba9d2f5
pphila <efl2ice89@gmail.com>
2023-02-02 19:51:37 -0800

    add list display to splash page, home controller to access db to display list and fixed typo in delete page

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,14 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace RecipeBox.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly BakeryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public HomeController(UserManager<ApplicationUser> userManager, BakeryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
 
     [HttpGet("/")]
-    public ActionResult Index()
+    public async Task<ActionResult> Index()
     {
-      return View();
+      Flavor[] flav = _db.Flavors.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("flavors", flav);
+      string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+      if (currentUser != null)
+      {
+        Treat[] treats = _db.Treats
+                        .Where(entry => entry.User.Id == currentUser.Id)
+                        .ToArray();
+        model.Add("treats", treats);
+      }
+      return View(model);
     }
     
   }
